c6591590802272f3315c6307fa35129d828ba6fd
John Tabash <jrtabash@yahoo.com>
2024-10-18 13:01:55 -0400

    Move CodeNode and BinaryOp to a bases file

--- src/libishlang/code_node.h
+++ src/libishlang/code_node.h
@@ -1,6 +1,7 @@
 #ifndef ISHLANG_CODE_NODE_H
 #define ISHLANG_CODE_NODE_H
 
+#include "code_node_bases.h"
 #include "environment.h"
 #include "hashtable.h"
 #include "instance.h"
@@ -10,52 +11,9 @@
 #include "value_pair.h"
 
 #include <functional>
-#include <memory>
-#include <optional>
-#include <vector>
 
 namespace Ishlang {
 
-    // -------------------------------------------------------------
-    class CodeNode {
-    public:
-        using SharedPtr      = std::shared_ptr<CodeNode>;
-        using SharedPtrList  = std::vector<SharedPtr>;
-        using SharedPtrPair  = std::pair<SharedPtr, SharedPtr>;
-        using SharedPtrPairs = std::vector<SharedPtrPair>;
-        using ParamList      = std::vector<std::string>;
-        using NameAndAsList  = std::vector<std::pair<std::string, std::optional<std::string>>>;
-        using NameSharedPtrs = std::vector<std::pair<std::string, SharedPtr>>;
-        
-    public:
-        CodeNode() {}
-        virtual ~CodeNode() {}
-        
-        Value eval(Environment::SharedPtr env) {
-            if (!env) { throw NullEnvironment(); }
-            return this->exec(env);
-        }
-
-    public:
-        template <typename CodeType, typename ... Args>
-        static SharedPtr make(Args && ... args) {
-            return std::make_shared<CodeType>(std::forward<Args>(args)...);
-        }
-
-    public:
-        virtual bool isIdentifier() const {
-            return false;
-        }
-
-        virtual const std::string &identifierName() const {
-            static const std::string Empty;
-            return Empty;
-        }
-
-    protected:
-        virtual Value exec(Environment::SharedPtr env) = 0;
-    };
-    
     // -------------------------------------------------------------
     class Literal : public CodeNode {
     public:
@@ -184,20 +142,6 @@ namespace Ishlang {
 
     private:
         CodeNode::SharedPtr code_;
-    };
-
-    // -------------------------------------------------------------
-    class BinaryOp : public CodeNode {
-    public:
-        BinaryOp(CodeNode::SharedPtr lhs, CodeNode::SharedPtr rhs) : lhs_(lhs), rhs_(rhs) {}
-        virtual ~BinaryOp() {}
-        
-    protected:
-        virtual Value exec(Environment::SharedPtr env) override = 0;
-        
-    protected:
-        CodeNode::SharedPtr lhs_;
-        CodeNode::SharedPtr rhs_;
     };
 
     // -------------------------------------------------------------
---
+++ src/libishlang/code_node_bases.h
@@ -0,0 +1,74 @@
+#ifndef ISHLANG_CODE_NODE_BASES_H
+#define ISHLANG_CODE_NODE_BASES_H
+
+#include "environment.h"
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace Ishlang {
+
+    // -------------------------------------------------------------
+    class CodeNode {
+    public:
+        using SharedPtr      = std::shared_ptr<CodeNode>;
+        using SharedPtrList  = std::vector<SharedPtr>;
+        using SharedPtrPair  = std::pair<SharedPtr, SharedPtr>;
+        using SharedPtrPairs = std::vector<SharedPtrPair>;
+        using ParamList      = std::vector<std::string>;
+        using NameAndAsList  = std::vector<std::pair<std::string, std::optional<std::string>>>;
+        using NameSharedPtrs = std::vector<std::pair<std::string, SharedPtr>>;
+
+    public:
+        CodeNode() {}
+        virtual ~CodeNode() {}
+
+        Value eval(Environment::SharedPtr env) {
+            if (!env) { throw NullEnvironment(); }
+            return this->exec(env);
+        }
+
+    public:
+        template <typename CodeType, typename ... Args>
+        static SharedPtr make(Args && ... args) {
+            return std::make_shared<CodeType>(std::forward<Args>(args)...);
+        }
+
+    public:
+        virtual bool isIdentifier() const {
+            return false;
+        }
+
+        virtual const std::string &identifierName() const {
+            static const std::string Empty;
+            return Empty;
+        }
+
+    protected:
+        virtual Value exec(Environment::SharedPtr env) = 0;
+    };
+
+    // -------------------------------------------------------------
+    class BinaryOp : public CodeNode {
+    public:
+        BinaryOp(CodeNode::SharedPtr lhs, CodeNode::SharedPtr rhs)
+            : CodeNode()
+            , lhs_(lhs)
+            , rhs_(rhs)
+        {}
+
+        virtual ~BinaryOp() {}
+
+    protected:
+        virtual Value exec(Environment::SharedPtr env) override = 0;
+
+    protected:
+        CodeNode::SharedPtr lhs_;
+        CodeNode::SharedPtr rhs_;
+    };
+
+}
+
+#endif	// ISHLANG_CODE_NODE_BASES_H
